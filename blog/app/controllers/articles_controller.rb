d11ee5a02f021818946b72c1bfaf1b1f77861524
Muji Girl <miki@fyi.tokyo>
2019-07-29 20:02:54 +0900

    Refactoring

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
   def index
     @articles = Article.all
   end
@@ -18,15 +20,12 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @aricle = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was updated"
       redirect_to article_path(@article)
@@ -34,6 +33,16 @@ class ArticlesController < ApplicationController
       flash[:notice] = "Article was not updated"
       render 'edit'
     end
+  end
+
+  def destroy
+    @article.destroy
+    flash[:notice] = "Article was deleted"
+    redirect_to articles_path
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 
   private
