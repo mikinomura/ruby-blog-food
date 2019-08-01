6c5ad0db375144dfcbb5b0636c22e08a32460c0c
Muji Girl <miki@fyi.tokyo>
2019-08-01 23:11:46 +0900

    Added username and timestamp in article page

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
-  validates: :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 25}
+  has_many :articles
+  validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 25}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates: :email, presence: {case_sensitive: false}, uniqueness: true, format: {with: VALID_EMAIL_REGEX}
+  validates :email, presence: {case_sensitive: false}, uniqueness: true, format: {with: VALID_EMAIL_REGEX}
   before_save {self.email = email.downcase}
 
 end
--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_01_134404) do
+ActiveRecord::Schema.define(version: 2019_08_01_140842) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
